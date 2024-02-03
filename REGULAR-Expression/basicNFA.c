2c7b21214766b7569b3b59ee8f4772a56005cd36
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-02-03 14:32:42 +0530

    Basic NFA taking one alphabet

--- REGULAR-Expression/basicNFA.c
+++ REGULAR-Expression/basicNFA.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 // Number of states in the NFA
-#define NUM_STATES 3
+#define NUM_STATES 2
 
 // Function to check if the string is accepted by the NFA
 bool isAccepted(const char *input) {
@@ -15,19 +15,14 @@ bool isAccepted(const char *input) {
         // Transition based on the current state and input symbol
         switch (currentState) {
             case 0:
-                if (symbol == '0') {
+                if (symbol == 'a') {
                     currentState = 1;
                 }
                 break;
             case 1:
-                if (symbol == '1') {
-                    currentState = 2;
-                } else if (symbol == '0') {
-                    currentState = 1;
+                if (symbol == 'a') {
+                    currentState = 0;
                 }
-                break;
-            case 2:
-                // Accepting state
                 break;
         }
 
@@ -36,12 +31,12 @@ bool isAccepted(const char *input) {
     }
 
     // Check if the final state is the accepting state
-    return currentState == 2;
+    return currentState == 1;
 }
 
 int main() {
     // Test the NFA with some input strings
-    const char *testStrings[] = {"0101", "001", "1001", "1010101", "011"};
+    const char *testStrings[] = {"a", "aa", "aaa", "aaaa", "aaaaa"};
 
     for (int i = 0; i < sizeof(testStrings) / sizeof(testStrings[0]); i++) {
         if (isAccepted(testStrings[i])) {
